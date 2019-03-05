d68595099cda695ffd2607aadb9d68a2731d4644
Patrick <bruijn.patrick@gmail.com>
2019-03-05 21:20:11 +0100

    only Azure credentials

--- pages/Config/Config.php
+++ pages/Config/Config.php
@@ -1,16 +1,9 @@
 <?php
 //                             * configuration file for the database *
-// This includes the online Azure credentials and localhost configuration (which is only useful for myself (Patrick)
-//So therefore, comment the localhost credentials and uncomment the Azure credentials.
+// This includes the online Azure credentials
 
-/*
 define('host', 'dbserverdscs.mysql.database.azure.com');
 define('user', 'dscadmin');
 define('pwd', 'dscs@UvA2019');
-define('db', 'taxidb'); */
-
-define('host', '127.0.0.1');
-define('user', 'admin');
-define('pwd', 'Welkom01');
-define('db', 'uber');
+define('db', 'taxidb');
 ?>
