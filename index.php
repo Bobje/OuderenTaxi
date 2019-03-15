c431eec838543915ca5a697fdd9bee66d1d6ae03
Joke <j.m.vanede@students.uu.nl>
2019-03-15 09:34:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,24 +9,33 @@ include 'pages/header.php';
 			
 			<div class="col-sm-8"> 
 				<form method="post" action="pages/address.php">
-					<div class="button_row">
-						<label class="form_label">Pick up time</label> 
-						
+					
+					<div class="col-sm-6 left button_row">
+					
 						<input type="radio" name="start_location" id="start_location_home" value="Now" checked> 
 						<label for="start_location_home">Now</label>
+					</div>
+
+					<div class="col-sm-6 right">
+						<div class="button_row">
+							<input type="radio" name="start_location" id="start_location_other" value="">							 
+							<label for="start_location_other">Plan a Taxi</label> 
+						</div>
+						<div class="button_row">
+							<label class="form_label">Date</label> 						
+							<input type="text" id="datepicker" name="input_date">
+							
 						
-						<input type="radio" name="start_location" id="start_location_other" value="">
-						<label for="start_location_other">Other</label> 
-						Date: <input type="text" id="datepicker">
-						<div class="md-form">
-						  <input placeholder="Selected time" type="text" id="input_starttime" class="form-control timepicker">
-						  <label for="input_starttime">Pickup time</label>
+							<label for="input_starttime">Pickup time</label>
+							<input type="time" id="input_starttime" name="input_starttime">
+								
+							
 						</div>
 
 					</div>
-					
+
 					<div class="button_row">
-						<input type="submit" value="Go To Step 2">
+						<button type="submit" class="registerbtn">Next step</button>
 					</div>
 				</form>
 			</div>
